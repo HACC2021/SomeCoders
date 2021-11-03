5f190cb22664cc7b5c3e95a4fe219c5c7fa1d0fb
bw-leran <70430003+bw-leran@users.noreply.github.com>
2021-11-02 19:07:59 -1000

    Add project files.

---
+++ HACC_PetPickupNotifications/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HACC_PetPickupNotifications.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
